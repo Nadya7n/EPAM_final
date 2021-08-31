e809018b0c5dee29a6b1a755a565bb9504428a95
Nadya7n <nadya7n@bk.ru>
2021-08-31 23:21:52 +0300

    Add README

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
 # EPAM_final
+## Browser code executor
+
+A web form for entering python code and executing it on the server side.
+
+### Structure 
+3 input fields:
+1. Python code (mandatory field) 
+2. Stdin (optional field)
+3. Timeout, but less than 5 seconds (optional field)
+
+When the program has finished - code, stdin, stdout and stderr will be displayed on our page. 
+
+You can edit your code after execution and run it again.
+
+### Important
+For security reasons, we do not allow "os" module and functions "exec" and "eval". "open" is allowed with read-only permissions.
+
+## Usage
+### Docker
+1. Download container image
+
+    
+    docker pull nadya7n/browser_code_executor:mytag
+
+2. Run container
+
+    
+    docker run nadya7n/browser_code_executor:mytag
+
+### Git
+1. Download project
+
+    
+    git clone https://github.com/Nadya7n/EPAM_final.git
+
+2. Install needed modules
+
+
+    pip install -r requirements.txt
+
+3. Run project
+
+
+    python3 browser_code_executor/main.py
+
--- browser_code_executor/main.py
+++ browser_code_executor/main.py
@@ -17,15 +17,20 @@ path_to_config = os.path.join(os.path.dirname(__file__), "config/config.ini")
 config.read(path_to_config)
 
 
+def return_timeout(config, new_value=0):
+    timeout = int(config["timeout"]["timeout"])
+    return timeout if new_value == 0 or int(new_value) > timeout else new_value
+
+
 class Table(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     code = db.Column(db.Text, nullable=False)
     stdin = db.Column(db.String(300), nullable=True)
     stderr = db.Column(db.Text, nullable=True)
     stdout = db.Column(db.Text, nullable=True)
+    timeout = db.Column(db.Integer, default=return_timeout(config))
 
     def __repr__(self):
-        # table object plus its id will be given out
         return "<Table %r>" % self.id
 
 
@@ -40,35 +45,32 @@ def create_file(code):
     return temp_file
 
 
-def parse_security():
-    path_to_file = os.path.join(os.path.dirname(__file__), "config/security.txt")
+def read_check_security_file():
+    path_to_file = os.path.join(os.path.dirname(__file__), "config/check_security.txt")
     with open(path_to_file) as fh:
         return fh.read()
 
 
-def run_script(code, stdin, config):
-    code_with_security_check = parse_security() + code
-    file = create_file(code_with_security_check)
+def run_script(code, stdin, timeout, config):
+    code_with_security_check = read_check_security_file() + code
+    script = create_file(code_with_security_check)
     args = [
         "python",
-        file,
+        script,
         config["black_list"]["functions"],
         config["black_list"]["imports"],
     ]
     process = Popen(args=args, stdin=PIPE, stdout=PIPE, stderr=PIPE, encoding="utf-8")
 
     try:
-        stdout, stderr = process.communicate(
-            stdin, timeout=int(config["timeout"]["timeout"])
-        )
+        stdout, stderr = process.communicate(stdin, timeout=timeout)
     except TimeoutExpired as to:
         stdout, stderr = "", f"TimeoutExpired: {to}"
     except Exception as ex:
         stdout, stderr = "", f"Error: {ex}"
     finally:
         table = Table(code=code, stdin=stdin, stdout=stdout, stderr=stderr)
-        os.remove(file)
-
+        os.remove(script)
     return table
 
 
@@ -77,7 +79,9 @@ def index():
     if request.method == "POST":
         code = request.form["code"]
         stdin = request.form["stdin"]
-        table = run_script(code, stdin, config)
+        input_timeout = request.form["timeout"] if request.form["timeout"] else 0
+        timeout = return_timeout(config, new_value=int(input_timeout))
+        table = run_script(code, stdin, timeout, config)
 
         try:
             db.session.add(table)
@@ -91,7 +95,6 @@ def index():
 
 @app.route("/database")
 def database():
-    # desc - reverse sort
     tables = Table.query.order_by(Table.id.desc()).all()
     return render_template("db.html", tables=tables)
 
@@ -119,8 +122,9 @@ def request_update(id):
     if request.method == "POST":
         table.code = request.form["code"]
         table.stdin = request.form["stdin"]
-
-        new_table = run_script(table.code, table.stdin, config)
+        input_timeout = request.form["timeout"] if request.form["timeout"] else 0
+        table.timeout = return_timeout(config, new_value=int(input_timeout))
+        new_table = run_script(table.code, table.stdin, table.timeout, config)
 
         table.stdout = new_table.stdout
         table.stderr = new_table.stderr
